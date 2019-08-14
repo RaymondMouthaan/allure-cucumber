5f7a5da06f36dc02e4058cb0afc2b465f4e3d3f7
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-08-14 23:56:56 +0200

    - screenshot fix

--- src/test/java/org/mouthaan/demo/hooks/Hooks.java
+++ src/test/java/org/mouthaan/demo/hooks/Hooks.java
@@ -2,14 +2,14 @@ package org.mouthaan.demo.hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import cucumber.api.java.Before;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.Before;
 
 public class Hooks {
 
     @Before
     public void setup() {
-        Configuration.reportsFolder = "target/results/selenide-screenshots";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+        Configuration.reportsFolder = "target/results/selenide-screenshots";
     }
 }
